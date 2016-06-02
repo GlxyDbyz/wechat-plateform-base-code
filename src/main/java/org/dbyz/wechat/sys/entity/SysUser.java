cf1ad66defb03464154663e9aaae3c576fb42957
GlxyDbyz <glxydbyz@gmail.com>
2016-06-02 16:05:54 +0800

    【BUG修改】序列化

--- src/main/java/org/dbyz/wechat/sys/entity/SysUser.java
+++ src/main/java/org/dbyz/wechat/sys/entity/SysUser.java
@@ -1,6 +1,9 @@
 package org.dbyz.wechat.sys.entity;
 
-public class SysUser {
+import java.io.Serializable;
+
+public class SysUser implements Serializable{
+	private static final long serialVersionUID = 1L;
 	private String name;
 	private String password;
 
